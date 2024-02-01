249bd65b784b70b27a8dfb735012e09b855a11df
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-02-01 23:31:51 +0530

    Add files via upload

---
+++ oop_in_python/file11.py
@@ -0,0 +1,59 @@
+class Employee:
+
+    raise_amount = 1.04
+    num_of_emps = 0
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + "@company.com"
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return "{} {}".format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+    def __repr__(self):
+        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
+
+
+emp_1 = Employee("Test", "User", 40000)
+emp_2 = Employee("Valid", "Username", 50000)
+
+# if we add these two Employee objects it returns the added salaries of the two employees
+print(emp_1 + emp_2)
+
+# the two print statements below are the same
+print(len("test"))
+print("test".__len__())
+
+# length of full name of employee
+print(len(emp_1))

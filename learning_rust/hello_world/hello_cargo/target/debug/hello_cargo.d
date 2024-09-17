9f7353f9107ea7fc42fbe24f0574da2b1847e385
Eizad Hamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-09-18 01:17:01 +0530

    Delete a direc

--- learning_rust/hello_world/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Games\python_texts\Bootcamp\Bootcamp\learning_rust\hello_world\hello_cargo\target\debug\hello_cargo.exe: C:\Games\python_texts\Bootcamp\Bootcamp\learning_rust\hello_world\hello_cargo\src\main.rs

a48566459ac24b9f8da5cb588f6855ebfb4033cf
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2025-04-06 13:07:04 +0530

    storing graphs

---
+++ Graphs/basics/file1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int n, m;
+    std::cin >> n >> m;
+
+    // storing graph here
+    int adj[n + 1][m + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        std::cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+    return 0;
+}
---
+++ Graphs/basics/file2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int n, m;
+    std::cin >> n >> m;
+
+    // storing graph here
+    std::vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        std::cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    return 0;
+}
